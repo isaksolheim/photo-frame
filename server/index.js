f569e107edd31ec5f58bc7f03b3837a020632506
wickdeeb <isak.m.h.s@gmail.com>
2020-08-22 12:41:30 +0200

    Add save image functionality

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ app.use(function(req, res, next) {
 app.post('/create', (req, res) => {
   images.push(req.body.data);
   res.send(JSON.stringify({ data: 'success' }));
+  saveImage(req.body.data);
 });
 
 app.get('/', (req, res) => {
@@ -28,3 +29,62 @@ app.get('/', (req, res) => {
 app.listen(3001, () => {
   console.log('server started on port 3001');
 });
+
+function decodeBase64Image(dataString) {
+  const matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+  let response = {};
+
+  if (matches.length !== 3) {
+    return new Error('Invalid input string');
+  }
+
+  response.type = matches[1];
+  response.data = new Buffer(matches[2], 'base64');
+
+  return response;
+}
+
+function saveImage(base64Data) {
+  try {
+    const imageTypeRegularExpression = /\/(.*?)$/;
+
+    const crypto = require('crypto');
+    const seed = crypto.randomBytes(20);
+    const uniqueSHA1String = crypto
+      .createHash('sha1')
+      .update(seed)
+      .digest('hex');
+
+    const imageBuffer = decodeBase64Image(base64Data);
+    const userUploadedFeedMessagesLocation = './img/';
+
+    const uniqueRandomImageName = 'image-' + uniqueSHA1String;
+
+    const imageTypeDetected = imageBuffer.type.match(
+      imageTypeRegularExpression
+    );
+
+    const userUploadedImagePath =
+      userUploadedFeedMessagesLocation +
+      uniqueRandomImageName +
+      '.' +
+      imageTypeDetected[1];
+
+    try {
+      require('fs').writeFile(
+        userUploadedImagePath,
+        imageBuffer.data,
+        function() {
+          console.log(
+            'DEBUG - feed:message: Saved to disk image attached by user:',
+            userUploadedImagePath
+          );
+        }
+      );
+    } catch (error) {
+      console.log('ERROR:', error);
+    }
+  } catch (error) {
+    console.log('ERROR:', error);
+  }
+}
